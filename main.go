fea3a5a7dd682298312d22d252dab6e455360282
Tyler Mikesell <tmikesell@clarityinnovates.com>
2019-07-23 12:20:16 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	botID	string
+)
+
+func main() {
+	discord, err := discordgo.New("Bot NjAzMTk3NTUwMzgzNzI2NjIy.XTb60g.Y-sTmVYTVVizn-eYq1DXJAXrano")
+	errCheck("error creating discord session", err)
+	user, err := discord.User("@me")
+	errCheck("error retrieving account", err)
+
+	botID = user.ID
+	discord.AddHandler(commandHandler)
+	discord.AddHandler(func(discord *discordgo.Session, ready *discordgo.Ready) {
+		err = discord.UpdateStatus(0, "Test Golang Bot")
+		if err != nil {
+			fmt.Println("Error attempting to set my status")
+		}
+		servers := discord.State.Guilds
+		fmt.Printf("GoBot has started on %d servers", len(servers))
+	})
+
+	err = discord.Open()
+	errCheck("Error opening connection to Discord", err)
+	defer discord.Close()
+
+	<-make(chan struct{})
+}
+
+func errCheck(msg string, err error) {
+	if err != nil {
+		fmt.Printf("%s: %+v", msg, err)
+		panic(err)
+	}
+}
+
+func commandHandler(discord *discordgo.Session, message *discordgo.MessageCreate) {
+	user := message.Author
+	if user.ID == botID || user.Bot {
+		//Do nothing because the bot is talking
+		return
+	}
+
+	content := message.Content
+
+	if content == "!Test" {
+		_, err := discord.ChannelMessageSend(message.ChannelID, "Testing...")
+		errCheck("Error sending message.", err)
+	}
+
+	fmt.Printf("Message: %+v || From: %s\n", message.Message, message.Author)
+}
